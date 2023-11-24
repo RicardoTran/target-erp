2d33562356086d3c03e7c9647f24aae5b383048e
Tran Manh Toan <toantm@ts68.vn>
2023-11-24 10:55:58 +0700

    Thay đổi nội dung push email sms

--- erpnext/selling/doctype/push_sms/push_sms.py
+++ erpnext/selling/doctype/push_sms/push_sms.py
@@ -18,18 +18,15 @@ class PushSMS(Document):
 		login_user = frappe.db.get_single_value("Push SMS Settings","user_name")
 		login_pass = get_decrypted_password("Push SMS Settings","Push SMS Settings","password")
 		service_id = frappe.db.get_single_value("Push SMS Settings","service_id")
-
-		#Get template
-		template = frappe.db.get_value('Push SMS Template', {'reference_type':self.reference_type}, ['content'], as_dict=1)
-		content = template.content.replace('{{link}}', self.url)
+		body = remove_accents(self.body)
 
 		#Send SMS
 		url = "https://ams.tinnhanthuonghieu.vn:8998/bulkapi?wsdl"
-		payload = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:impl=\"http://impl.bulkSms.ws/\">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <impl:wsCpMt>\n         <!--Optional:-->\n         <User>" + login_user + "</User>\n         <!--Optional:-->\n         <Password>" + login_pass + "</Password>\n         <!--Optional:-->\n         <CPCode>" + service_id + "</CPCode>\n         <!--Optional:-->\n         <RequestID>1</RequestID>\n         <!--Optional:-->\n         <UserID>" + self.phone_number + "</UserID>\n         <!--Optional:-->\n         <ReceiverID>" + self.phone_number + "</ReceiverID>\n         <!--Optional:-->\n         <ServiceID>" + service_id + "</ServiceID>\n         <!--Optional:-->\n         <CommandCode>bulksms</CommandCode>\n         <!--Optional:-->\n         <Content>" + content + "</Content>\n         <!--Optional:-->\n         <ContentType>" + str(self.unicode_char) + "</ContentType>\n      </impl:wsCpMt>\n   </soapenv:Body>\n</soapenv:Envelope>\n"
+		payload = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:impl=\"http://impl.bulkSms.ws/\">\n   <soapenv:Header/>\n   <soapenv:Body>\n      <impl:wsCpMt>\n         <!--Optional:-->\n         <User>" + login_user + "</User>\n         <!--Optional:-->\n         <Password>" + login_pass + "</Password>\n         <!--Optional:-->\n         <CPCode>" + service_id + "</CPCode>\n         <!--Optional:-->\n         <RequestID>1</RequestID>\n         <!--Optional:-->\n         <UserID>" + self.phone_number + "</UserID>\n         <!--Optional:-->\n         <ReceiverID>" + self.phone_number + "</ReceiverID>\n         <!--Optional:-->\n         <ServiceID>" + service_id + "</ServiceID>\n         <!--Optional:-->\n         <CommandCode>bulksms</CommandCode>\n         <!--Optional:-->\n         <Content>" + body + "</Content>\n         <!--Optional:-->\n         <ContentType>" + str(self.unicode_char) + "</ContentType>\n      </impl:wsCpMt>\n   </soapenv:Body>\n</soapenv:Envelope>\n"
 		headers = {
 			'Content-Type': 'text/xml'
 		}
-		response = requests.request("POST", url, headers=headers, data=payload)
+		response = requests.request("POST", url, headers=headers, data=payload, )
 
 		frappe.get_doc({
 			'doctype': 'Comment',
@@ -42,6 +39,17 @@ class PushSMS(Document):
 			self.result = 1
 		else:
 			self.result = 0
-		self.content = content
 		self.save(ignore_permissions=True)
 		# self.submit()
+
+def remove_accents(input_str):
+	s1 = u'ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ'
+	s0 = u'AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy'
+	s = ''
+	input_str.encode('utf-8')
+	for c in input_str:
+		if c in s1:
+			s += s0[s1.index(c)]
+		else:
+			s += c
+	return s
