beeebf584f1d3110a79a6845c93824fee0efec28
Tran Manh Toan <toantm@ts68.vn>
2024-01-03 16:03:33 +0700

    Gửi sms tiếng việt có dấu

--- erpnext/crm/doctype/contract/contract.py
+++ erpnext/crm/doctype/contract/contract.py
@@ -232,7 +232,7 @@ class Contract(Document):
 				push_sms = frappe.new_doc('Push SMS')
 				push_sms.phone_number = x.replace('0','84', 1).replace(' ','')
 				push_sms.body = body
-				push_sms.unicode_char = 0
+				push_sms.unicode_char = 1
 				push_sms.reference_type = "Contract"
 				push_sms.reference_name = self.name
 				push_sms.url = approval_url
